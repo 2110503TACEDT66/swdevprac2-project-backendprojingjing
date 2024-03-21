8e14644f9946031713e7e6e346d32ed46ea9b35e
NattawitMana <6633066721@student.chula.ac.th>
2024-03-21 16:33:58 +0700

    change bg

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,21 @@
+'use client'
 import styles from './banner.module.css'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Banner(){
+
+    const covers = ['/img/cover1.jpg','/img/cover2.jpg','/img/cover3.jpg','/img/cover4.jpg']
+    const [index, setIndex] = useState(0)
+
     return(
-        <div className={styles.banner}>
-            <Image src={'/img/main1.jpg'}
+        <div className='block p-[5px] m-0 w-[100vw] h-[90vh] relative' onClick={()=>{setIndex(index+1)}}>
+            <Image src={covers[index%4]}
             alt='coworkingspace'
             fill={true}
             priority
-            objectFit='cover'/>
+            // objectFit='cover'
+            />
             <div className='relative top-[100px] z-20 text-center bg-gray-50/70 mx-[15%]'>
                 <h1 className='text-4xl font-medium text-black bg-transparent-400'>
                     Co-working Space Reservation
