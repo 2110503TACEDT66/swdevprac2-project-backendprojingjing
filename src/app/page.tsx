8a6a4d08694b9be14d7e1bd5ff0add8a7d104253
NattawitMana <6633066721@student.chula.ac.th>
2024-03-21 17:01:16 +0700

    finish A8 (no hooks)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import styles from './page.module.css'
 import Banner from '@/components/Banner'
 import Card from "@/components/Card"
 import CardPanel from '@/components/CardPanel'
+import PromoteCard from '@/components/PromoteCard'
 
 export default function Home() {
   return (
     <main>
       <Banner/>
-      <CardPanel/>
+      <PromoteCard/>  
     </main>
   )
 }
