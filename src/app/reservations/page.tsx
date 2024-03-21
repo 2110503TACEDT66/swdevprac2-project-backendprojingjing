d4e7e2cccd44053e4664828db84ad670428ad14c
NattawitMana <6633066721@student.chula.ac.th>
2024-03-21 17:21:23 +0700

    add logo

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -4,10 +4,9 @@ import TimeReserve from '@/components/TimeReserve'
 
 export default function Reservation(){
     return(
-        <form>
-        <main className="w-[100%] flex flex-col space-y-4 bg-white text-black-600 border border-gray-600
-            font-semibold m-20 p-10 rounded-[5%] flex justify-center items-center" >
-            
+        <main >
+            <form className="w-[100%] flex flex-col space-y-4 bg-white text-black-600 border border-gray-600
+            font-semibold m-20 p-10 rounded-[5%] flex justify-center items-center shadow-xl" >
             <div className="text-x1 font-medium text-black text-3xl">Co-Workingspace Reservation</div>
             <br/>
                 <label htmlFor="Name-Lastname" className="Name-Lastname" id="reserveform">Name-Lastname</label>
@@ -25,7 +24,7 @@ export default function Reservation(){
                 className="h-[2em] w-[200px] border-2 p-3">
                 <MenuItem value="Cospace1">Co-working space1 </MenuItem>
                 <MenuItem value="Cospace2">Co-working space2 </MenuItem>
-                <MenuItem value="Cospace3">Co-working space3</MenuItem>
+                <MenuItem value="Cospace3">Co-working space3 </MenuItem>
                 </Select>
                 <br/>
                 <label htmlFor="Reservation Date" className="Reservation Date" id="reserveform">Reservation Date</label>
@@ -46,7 +45,7 @@ export default function Reservation(){
                 <button type="submit" className="block rounded-md bg-sky-500 hover:bg-indigo-500 delay-100 px-3 py-2 shadow-sm">
                     Reserve Co-workingspace
                 </button>
+                </form>
         </main>
-        </form>
     );
 }
