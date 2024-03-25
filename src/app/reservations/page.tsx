07233029f9f90cd6802100d175fe4fec2b5583ae
Siamrat27 <147132780+Siamrat27@users.noreply.github.com>
2024-03-25 15:25:32 +0700

    datetimereserve

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -18,38 +18,14 @@ export default async function Reservation(){
 
             <form className="w-[100%] flex flex-col space-y-4 bg-white text-black-600 border border-gray-600
             font-semibold m-20 p-10 rounded-[5%] flex justify-center items-center shadow-xl" >
-            <div className="text-x1 font-medium text-black text-3xl">Co-Workingspace Reservation</div>
-            <br/>
-                <label htmlFor="Name-Lastname" className="Name-Lastname" id="reserveform">Name-Lastname</label>
-                <input type="text" id="Name-Lastname" placeholder="Name-Lastname" 
-                className="MuiInput-input border-2 p-3"></input>
-                <br/>
-
-                <label htmlFor="Citizen ID" className="Citizen ID" id="reserveform">Citizen ID</label>
-                <input type="text" id="Citizen ID" placeholder="Citizen ID" 
-                className="MuiInput-input border-2 p-3"></input>
-                <br/>
-                
-                <label htmlFor="coworkingspace" className="MuiSelect-select'" id="reserveform">Coworkingspace</label>
-                <Select variant="standard" name="coworkingspace" id="coworkingspace"
-                className="h-[2em] w-[200px] border-2 p-3">
-                <MenuItem value="Cospace1">Co-working space1 </MenuItem>
-                <MenuItem value="Cospace2">Co-working space2 </MenuItem>
-                <MenuItem value="Cospace3">Co-working space3 </MenuItem>
-                </Select>
-                <label htmlFor="Reservation Date" className="Reservation Date" id="reserveform">Reservation Date and Time</label>
                 <div className="w-fit space space-y-1">
                     <div className="text-md text-left text-gray-600"></div>
                     <DateTimeReserve/>
                 </div>
-
-                <br />             
-
                 <button type="submit" className="block rounded-md bg-sky-500 hover:bg-indigo-500 delay-100 px-3 py-2 shadow-sm">
                     Reserve Co-workingspace
                 </button>
-                </form>
-
+            </form>
                 <div>
                 <table className="table-auto border-separate border-spacing-2 mt-[45%] border-2
                 p-5 pr-10 w-3/6">
--- src/components/DateTimeReserve.tsx
+++ src/components/DateTimeReserve.tsx
@@ -10,8 +10,37 @@ import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 export default function DateReserve(){
     return(
-        <div className="bg-white rounded-1g space-x-5 space-y-2 w-fit px-10 py-5
-        flex flex-row justify-center">
+        <div className="text-center m-2">
+            <div className="text-x1 font-medium text-black text-3xl">Co-Workingspace Reservation</div>
+            <br/>
+                <label htmlFor="Name-Lastname" className="Name-Lastname" id="reserveform">Name-Lastname</label>
+                <input type="text" id="Name-Lastname" placeholder="Name-Lastname" 
+                className="MuiInput-input border-2 p-3"></input>
+                <br/>
+                <br/>
+                <label htmlFor="Citizen ID" className="Citizen ID" id="reserveform">Citizen ID</label>
+                <input type="text" id="Citizen ID" placeholder="Citizen ID" 
+                className="MuiInput-input border-2 p-3"></input>
+                <br/>
+                <br/>
+                <label htmlFor="coworkingspace" className="MuiSelect-select'" id="reserveform">Coworkingspace</label>
+                <Select variant="standard" name="coworkingspace" id="coworkingspace"
+                className="h-[2em] w-[200px] border-2 p-3">
+                <MenuItem value="Nexus CoLab">Nexus CoLab </MenuItem>
+                <MenuItem value="Hive Workspace">Hive Workspace </MenuItem>
+                <MenuItem value="Innovate Hub">Innovate Hub </MenuItem>
+                <MenuItem value="FlexiSpaceCospace2">FlexiSpace </MenuItem>
+                <MenuItem value="CollaboraZone">CollaboraZone </MenuItem>
+                <MenuItem value="Unity Workspace">Unity Workspace </MenuItem>
+                <MenuItem value="Synergy Station">Synergy Station </MenuItem>
+                <MenuItem value="Innovate Loft">Innovate Loft </MenuItem>
+                <MenuItem value="Collaborate Corner">Collaborate Corner </MenuItem>
+                <MenuItem value="Harmony Hub">Harmony Hub </MenuItem>
+                </Select>
+                <br/>
+                <br/>
+                <label htmlFor="Reservation Date" className="Reservation Date" id="reserveform">Reservation Date and Time</label>
+                            
                 <div>
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DatePicker className="bg-white "/>
