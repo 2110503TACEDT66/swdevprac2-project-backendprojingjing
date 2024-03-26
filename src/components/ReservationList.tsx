dfe05e59d5963536edc93437c5f14fa3b4f259f6
Siamrat27 <147132780+Siamrat27@users.noreply.github.com>
2024-03-26 23:28:31 +0700

    test

--- src/components/ReservationList.tsx
+++ src/components/ReservationList.tsx
@@ -44,7 +44,6 @@ export default function ReservationList({ reservationJson }: { reservationJson: 
             console.error('Error deleting reservation:', error);
         }
     };
-
     return (
         <>
             {reservingJsonReady && (
